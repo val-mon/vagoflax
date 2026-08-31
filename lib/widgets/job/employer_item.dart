f8540e0e97a44eb9b697748d35829d18470bcdcf
g-ae <goncalo.arieira@hotmail.com>
2026-08-31 09:48:33 +0200

    fix: description and titles too big

--- lib/widgets/job/employer_item.dart
+++ lib/widgets/job/employer_item.dart
@@ -17,9 +17,11 @@ class JobEmployerItem extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: ListTile(
-        title: Text(job.title),
+        title: Text(job.title, maxLines: 1, overflow: TextOverflow.ellipsis),
         subtitle: Text(
           job.description == '' ? 'No description available' : job.description!,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
         ),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
