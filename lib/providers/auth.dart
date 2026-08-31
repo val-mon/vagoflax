3de66bf0572b2769d345524c6d748e51258e1e2c
g-ae <goncalo.arieira@hotmail.com>
2026-08-31 09:17:59 +0200

    ref: removed firebase code from app_state

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -6,9 +6,11 @@ import 'package:firebase_auth/firebase_auth.dart'
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+
 import 'package:vagoflax/models/enum/user_role.dart';
 import 'package:vagoflax/providers/user.dart';
 import 'package:vagoflax/models/history.dart';
+
 
 import 'package:vagoflax/services/cloudinary.dart';
 import 'package:vagoflax/models/user.dart' as usermodel;
